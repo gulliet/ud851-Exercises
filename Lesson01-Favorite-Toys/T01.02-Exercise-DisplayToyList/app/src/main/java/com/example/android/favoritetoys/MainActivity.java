b4a0f3f6bc0c2aa1c770dec27dd019359b7c8eff
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-11-16 10:16:09 +0100

    Loop through each toy and append the name to the TextView (add \n for spacing)

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         String [] toyNames = ToyBox.getToyNames();
 
-        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        for (String toyName : toyNames) {
+            mToysListTextView.append(toyName + "\n\n\n");
+        }
     }
 }
