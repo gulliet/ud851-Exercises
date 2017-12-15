783e7da8eb448ec50f42acf83a13fda7777d2e7e
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-15 11:27:26 +0100

    Use findViewById to store a reference to the RecyclerView in mNumbersList

--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -35,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        // completed (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
 
         // TODO (5) Create a LinearLayoutManager variable called layoutManager
         // TODO (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
