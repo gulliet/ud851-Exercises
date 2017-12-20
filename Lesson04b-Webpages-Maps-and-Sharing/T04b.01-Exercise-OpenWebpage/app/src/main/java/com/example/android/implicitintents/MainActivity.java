101ab52f7f317ba4aaa71a4b09505cff30f2e0b1
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-20 15:55:49 +0100

    Replace the Toast with a call to openWebPage, passing in the URL String from the previous step

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         // completed (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
         String urlAsString = "http://www.udacity.com";
 
-        // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        // completed (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
+        openWebPage(urlAsString);
     }
 
     /**
