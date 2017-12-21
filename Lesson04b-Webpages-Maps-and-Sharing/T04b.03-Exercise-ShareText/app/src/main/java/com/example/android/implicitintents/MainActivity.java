25dc260c164548d2669ece8cf9428dc44a1c09c0
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2017-12-21 23:21:46 +0100

    Create a void method called shareText that accepts a String as a parameter

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -143,12 +143,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (1) Create a void method called shareText that accepts a String as a parameter
+    // completed (1) Create a void method called shareText that accepts a String as a parameter
     // Do steps 2 - 4 within the shareText method
+    public void shareText(String text) {
 
         // TODO (2) Create a String variable called mimeType and set it to "text/plain"
 
         // TODO (3) Create a title for the chooser window that will pop up
 
         // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+    }
 }
