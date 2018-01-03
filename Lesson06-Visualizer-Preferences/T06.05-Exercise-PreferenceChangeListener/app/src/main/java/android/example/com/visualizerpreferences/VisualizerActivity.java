83122c0c86d18369f009388a8a9fb9ad5d9d7e7d
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-03 19:25:19 +0100

    Register the listener

--- Lesson06-Visualizer-Preferences/T06.05-Exercise-PreferenceChangeListener/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.05-Exercise-PreferenceChangeListener/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -59,7 +59,8 @@ public class VisualizerActivity extends AppCompatActivity
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
         mVisualizerView.setColor(getString(R.string.pref_color_red_value));
-        // TODO (3) Register the listener
+        // completed (3) Register the listener
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     // completed (2) Override the onSharedPreferenceChanged method and update the show bass preference
