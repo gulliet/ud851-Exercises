0372f9a09f88757d3434e5eb42b112ff7a422bfe
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-03 17:17:16 +0100

    Get the value of the show_bass checkbox preference and use it to call setShowBass

--- Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -55,7 +55,7 @@ public class VisualizerActivity extends AppCompatActivity {
                 PreferenceManager.getDefaultSharedPreferences(this);
 
         // TODO (3) Get the value of the show_bass checkbox preference and use it to call setShowBass
-        mVisualizerView.setShowBass(true);
+        mVisualizerView.setShowBass(sharedPreferences.getBoolean("show_bass", true));
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
