1a732a4d0119e39034d1eb5248f7d11869c071e5
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-13 16:03:00 +0100

    Inside create a static final members for the table name and each of the db columns

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -10,11 +10,11 @@ public final class WaitlistContract {
     // completed (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
     public static class WaitlistEntry implements BaseColumns {
 
-        // TODO (2) Inside create a static final members for the table name and each of the db columns
-        // TABLE_NAME -> waitlist;
-        // COLUMN_GUEST_NAME -> guestName
-        // COLUMN_PARTY_SIZE -> partySize
-        // COLUMN_TIMESTAMP -> timestamp
+        // completed (2) Inside create a static final members for the table name and each of the db columns
+        public static final String TABLE_NAME = "waitlist";
+        public static final String COLUMN_GUEST_NAME = "guestName";
+        public static final String COLUMN_PARTY_SIZE = "partySize";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
 
     }
 
