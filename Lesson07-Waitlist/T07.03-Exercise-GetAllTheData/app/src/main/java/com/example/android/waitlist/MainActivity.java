ce65240eb979c8e4f917784fd953e4b0232ae03a
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-13 23:13:38 +0100

    Create a local field member of type SQLiteDatabase called mDb

--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.waitlist;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private GuestListAdapter mAdapter;
 
-    // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+    // completed (1) Create a local field member of type SQLiteDatabase called mDb
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
