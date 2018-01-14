715501b3a6a5795765f9ab05d25cf8fee608aa1a
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-14 18:29:03 +0100

    Retrieve the id from the cursor and

--- Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -44,7 +44,8 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         // Update the view holder with the information needed to display
         String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
         int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
-        // TODO (6) Retrieve the id from the cursor and
+        // completed (6) Retrieve the id from the cursor and
+        long id = mCursor.getLong(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry._ID));
 
         // Display the guest name
         holder.nameTextView.setText(name);
