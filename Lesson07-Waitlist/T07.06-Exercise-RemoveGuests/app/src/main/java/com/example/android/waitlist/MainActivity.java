b4b241e6d07d9ce4c650cac089eb2da187567be5
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-14 18:06:07 +0100

    Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id

--- Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.06-Exercise-RemoveGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -139,7 +139,9 @@ public class MainActivity extends AppCompatActivity {
     // completed (1) Create a new function called removeGuest that takes long id as input and returns a boolean
     private boolean removeGuest(long id) {
 
-        // TODO (2) Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id
+        // completed (2) Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id
+        return mDb.delete(WaitlistContract.WaitlistEntry.TABLE_NAME,
+                WaitlistContract.WaitlistEntry._ID + "=" + id, null) > 0;
     }
 
 
