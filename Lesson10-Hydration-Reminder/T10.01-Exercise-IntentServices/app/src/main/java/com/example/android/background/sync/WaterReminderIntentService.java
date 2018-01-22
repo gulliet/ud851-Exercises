0ab581ee4a21459cec9151c4e3e7276ceb600aad
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-22 16:06:10 +0100

    Create WaterReminderIntentService and extend it from IntentService

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,3 +1,5 @@
+package com.example.android.background.sync;
+
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -14,10 +16,24 @@
  * limitations under the License.
  */
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+// completed (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
+    //  completed (10) Create a default constructor that calls super with the name of this class
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+//  completed (11) Override onHandleIntent
+//      completed (12) Get the action from the Intent that started this Service
+//      completed (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+}
