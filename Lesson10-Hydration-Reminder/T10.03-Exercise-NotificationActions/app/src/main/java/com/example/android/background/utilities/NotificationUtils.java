022ba08109426a8cd867b8fbe89165a7d1c686fc
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-01-24 16:35:11 +0100

    Add a static method called drinkWaterAction

--- Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ Lesson10-Hydration-Reminder/T10.03-Exercise-NotificationActions/app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -119,12 +119,14 @@ public class NotificationUtils {
     }
 
 
-    //  TODO (11) Add a static method called drinkWaterAction
-    //      TODO (12) Create an Intent to launch WaterReminderIntentService
-    //      TODO (13) Set the action of the intent to designate you want to increment the water count
-    //      TODO (14) Create a PendingIntent from the intent to launch WaterReminderIntentService
-    //      TODO (15) Create an Action for the user to tell us they've had a glass of water
-    //      TODO (16) Return the action
+    //  completed (11) Add a static method called drinkWaterAction
+    private static NotificationCompat.Action drinkWaterAction(Context context) {
+        //      TODO (12) Create an Intent to launch WaterReminderIntentService
+        //      TODO (13) Set the action of the intent to designate you want to increment the water count
+        //      TODO (14) Create a PendingIntent from the intent to launch WaterReminderIntentService
+        //      TODO (15) Create an Action for the user to tell us they've had a glass of water
+        //      TODO (16) Return the action
+    }
 
 
     private static PendingIntent contentIntent(Context context) {
